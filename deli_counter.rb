9a6d22f55d8bb0fa21d04470ff8a548586671c02
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-02-24 03:32:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    tag="The line is currently:"
+
+    katz_deli.each_with_index do |value, index|
+      tag += " #{index.to_i+1}. #{value}"
+    end
+
+    puts "#{tag}"
+  end
